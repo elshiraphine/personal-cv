b0110eb0e503621d713961e572d5035738573b30
callmetaka <erviana.205025@mhs.its.ac.id>
2022-02-11 17:50:08 +0700

    feat: add post method handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import qs from 'query-string'
 
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 8000;
@@ -9,6 +10,7 @@ const server = http.createServer((req, res) => {
      if(req.method === 'GET') {
           handleGet(req, res);
      } else if (req.method === 'POST'){
+          // console.log(req);
           handlePost(req, res);
      } else res.writeHead(405).end(`{"error":"${http.STATUS_CODES[405]}}`);
 });
@@ -18,10 +20,10 @@ server.listen(port, () => {
 });
 
 function handleGet(req, res){
-     if(req.url == "/") req.url = "src/index.html";
-     let filePath = '/' + req.url;
+     if(req.url == "/") req.url = "/index.html";
+     let filePath = 'public' + req.url;
 
-     console.log(filePath);
+     // console.log(filePath);
 
      let extension = path.extname(filePath);
      if(extension == "") filePath += ".html";
@@ -46,12 +48,15 @@ function handleGet(req, res){
           case '.wav':
                contentType = 'audio/wav';
                break;
+          case '.svg':
+               contentType = 'image/svg+xml';
+               break
      }
 
      fs.readFile(filePath, function(error, content){
           if (error){
                if(error.code == 'ENOENT'){
-                    res.writeHead(404);
+                    res.writeHead(400);
                     res.end();
                } else {
                     res.writeHead(500);
@@ -64,3 +69,49 @@ function handleGet(req, res){
           }
      })
 }
+
+function handlePost(req, res){
+     let body = []
+     if (req.url != '/rmo'){
+         return res.writeHead(404).end();
+     }
+     req.on('data', (chunk) => {
+         body.push(chunk);
+     }).on('end', ()=>{
+         body = Buffer.concat(body).toString();
+         res.on('error',(err)=>{
+             console.error(err);
+         })
+ 
+         res.writeHead(200,{
+             'Content-Type': 'application/json',
+         })
+         console.log(body);
+         const object = qs.parse(body, "&","=");
+         console.log(object);
+         console.log(Object.keys(object).length);
+         if(!object.name || !object.email || !object.mes 
+             || Object.keys(object).length != 3) {
+             res.writeHead(406).end("Data Not Acceptable");
+         }
+         writeObj(object,"contact.json");
+         res.writeHead(200).end("success");
+     })
+}
+
+function writeObj(object, file){
+     fs.readFile(file, 'utf8', (err, data) => {
+         if (err) {
+             console.log(err);
+         } else {
+             let obj = [];
+             obj = JSON.parse(data);
+             obj.push(object);
+             let json = JSON.stringify(obj);
+             fs.writeFile(file ,json, 'utf8', err => {
+                 if(err)console.log(err);
+             })
+         }
+     })
+ }
+//  function
